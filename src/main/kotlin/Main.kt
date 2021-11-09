c28ce6579c6c4919612ab35f6fe67db94e4dadd8
Alejandro Andrade <soyalextreme@gmail.com>
2021-11-09 14:50:28 -0600

    Types of the variables [add] Types of variables on Kotlin. [add] Template literals. [add] Main function of the variables.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,4 +17,27 @@ fun main(args: Array<String>) {
 //    namePerson = "Jona" -> error solo variable de lectura
     println(namePerson)
     println(PI)
+
+    val odd : Boolean = true
+    // soporte para numeros mas grandes
+    val numberLong: Long = 3L
+    // Flotantes .
+    val double: Double = 2.2131
+    // Porcentaje de luminasidad
+    val float: Float = 2.31f
+
+    val firstValue = 20
+    val secondValue = 10
+//    val thirdValue = firstValue.minus(secondValue)
+    val result = firstValue - secondValue
+    println(result)
+
+    // Kotlin all is an object, this objects are transformed to
+    // primitive variables en time execution if the variable is not a null type
+    val name: String = "Alejandro"
+    val lastName: String = "Andrade"
+    val age: Int = 23
+    //! template string
+    val fullName: String = "My name is $name $lastName and Im $age old"
+    println(fullName)
 }
